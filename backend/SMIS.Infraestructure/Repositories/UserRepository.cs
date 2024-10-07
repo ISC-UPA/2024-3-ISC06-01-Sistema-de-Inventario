6c7037295ab916e1be359dc78e34fee79657702e
Nico Quintanilla <up220168@alumnos.upa.edu.mx>
2024-10-06 21:08:39 -0600

    Estructura backend Stock-Master Inventory System

---
+++ backend/SMIS.Infraestructure/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using SMIS.Core.Entities;
+using SMIS.Core.Interfaces;
+using SMIS.Infraestructure.Data;
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMIS.Infraestructure.Repositories
+{
+    public class UserRepository: IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
