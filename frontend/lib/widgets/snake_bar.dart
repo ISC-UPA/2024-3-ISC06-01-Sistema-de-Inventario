650f5cf4858f7d142d45ebbf1ffb7b2ec7120036
UP220672 <up220672@alumnos.upa.edu.mx>
2024-10-13 09:12:12 -0600

    Se modifico el SankeBar para tener diferentes tamaños en escritorio y en telefonos

--- frontend/lib/widgets/snake_bar.dart
+++ frontend/lib/widgets/snake_bar.dart
@@ -9,18 +9,21 @@ class CustomSnackBar {
 
       final color = Theme.of(context).colorScheme;
 
+      // Determina si es un dispositivo móvil o de escritorio
+      final isMobile = MediaQuery.of(context).size.width < 600; // Puedes ajustar este valor según tus necesidades
+
       final snackBar = SnackBar(
+        width: isMobile ? 300 : 450, // 50% del ancho para móvil o 400px para escritorio
         content: Text(
           message,
           style: const TextStyle(fontSize: 16, color: Colors.white),
         ),
         backgroundColor: color.primary,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
+          borderRadius: BorderRadius.circular(10),
         ),
         behavior: SnackBarBehavior.floating,
-        margin: const EdgeInsets.only(left: 20, bottom: 20), // Ajuste para que aparezca en la esquina inferior izquierda
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10), // Padding para un mejor ajuste del texto
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15), // Padding para un mejor ajuste del texto
         duration: const Duration(seconds: 3),
       );
 
