231a84fe268923913420aba26840fe0320470b94
UP220672 <up220672@alumnos.upa.edu.mx>
2024-11-30 11:43:56 -0600

    Enhance user interface responsiveness and improve data handling in product, employee, and supplier views

--- frontend/lib/responsive/desktop/ordenes.dart
+++ frontend/lib/responsive/desktop/ordenes.dart
@@ -20,21 +20,22 @@ class _OrderDesktopState extends State<OrderDesktop> {
     'Cerrado',
     'Cancelado'
   ];
-  late List<int> allTickets;
+  late List<Map<String, dynamic>> allTickets;
 
   @override
   void initState() {
     super.initState();
-    allTickets = List<int>.generate(7, (index) => index + 1);
+    allTickets = List<Map<String, dynamic>>.generate(7, (index) => {
+      'number': index + 1,
+      'type': categories[(index + 1) % categories.length]
+    });
   }
 
-  List<int> getFilteredTickets() {
+  List<Map<String, dynamic>> getFilteredTickets() {
     if (selectedCategoryIndex == 0) {
       return allTickets;
-    } else if (selectedCategoryIndex == 1) {
-      return allTickets.where((ticket) => ticket % 2 != 0).toList();
     } else {
-      return allTickets.where((ticket) => ticket % 2 == 0).toList();
+      return allTickets.where((ticket) => ticket['type'] == categories[selectedCategoryIndex]).toList();
     }
   }
 
@@ -52,7 +53,8 @@ class _OrderDesktopState extends State<OrderDesktop> {
 
   @override
   Widget build(BuildContext context) {
-    List<int> filteredTickets = getFilteredTickets();
+    final theme = Theme.of(context).colorScheme;
+    List<Map<String, dynamic>> filteredTickets = getFilteredTickets();
     double screenWidth = MediaQuery.of(context).size.width;
     int crossAxisCount = calculateCrossAxisCount(screenWidth);
 
@@ -65,8 +67,7 @@ class _OrderDesktopState extends State<OrderDesktop> {
                 const DesktopMenu(),
                 Expanded(
                   child: Padding(
-                    padding:
-                        const EdgeInsets.only(top: 20, left: 20, right: 20),
+                    padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
                     child: Column(
                       children: [
                         Center(
@@ -76,24 +77,28 @@ class _OrderDesktopState extends State<OrderDesktop> {
                               scrollDirection: Axis.horizontal,
                               child: Row(
                                 mainAxisAlignment: MainAxisAlignment.center,
-                                children:
-                                    List.generate(categories.length, (index) {
+                                children: List.generate(categories.length, (index) {
                                   return Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 8.0),
+                                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
                                     child: ElevatedButton(
                                       style: ElevatedButton.styleFrom(
-                                        backgroundColor:
-                                            selectedCategoryIndex == index
-                                                ? Colors.blue
-                                                : Colors.grey,
+                                        backgroundColor: selectedCategoryIndex == index
+                                            ? theme.primary
+                                            : theme.surface,
                                       ),
                                       onPressed: () {
                                         setState(() {
                                           selectedCategoryIndex = index;
                                         });
                                       },
-                                      child: Text(categories[index]),
+                                      child: Text(
+                                        categories[index],
+                                        style: TextStyle(
+                                          color: selectedCategoryIndex == index
+                                              ? theme.onPrimary
+                                              : theme.onSurface,
+                                        ),
+                                      ),
                                     ),
                                   );
                                 }),
@@ -104,8 +109,7 @@ class _OrderDesktopState extends State<OrderDesktop> {
                         const SizedBox(height: 20),
                         Expanded(
                           child: GridView.builder(
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
+                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                               crossAxisCount: crossAxisCount,
                               crossAxisSpacing: 30.0,
                               mainAxisSpacing: 30.0,
@@ -119,7 +123,10 @@ class _OrderDesktopState extends State<OrderDesktop> {
                                 padding: const EdgeInsets.all(20),
                                 child: SingleChildScrollView(
                                   child: TicketData(
-                                      ticketNumber: filteredTickets[index]),
+                                    ticketNumber: filteredTickets[index]['number'],
+                                    theme: theme,
+                                    ticketType: filteredTickets[index]['type'], // Pass the selected category as ticket type
+                                  ),
                                 ),
                               );
                             },
@@ -141,106 +148,143 @@ class _OrderDesktopState extends State<OrderDesktop> {
 
 class TicketData extends StatelessWidget {
   final int ticketNumber;
+  final ColorScheme theme;
+  final String ticketType;
 
-  const TicketData({super.key, required this.ticketNumber});
+  const TicketData({super.key, required this.ticketNumber, required this.theme, required this.ticketType});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: 120.0,
-              height: 25.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30.0),
-                border: Border.all(width: 1.0, color: Colors.green),
-              ),
-              child: const Center(
-                child: Text(
-                  'Business Class',
-                  style: TextStyle(color: Colors.green),
-                ),
-              ),
-            ),
-            const Row(
-              children: [
-                Text(
-                  'LHR',
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 8.0),
-                  child: Icon(
-                    Icons.flight_takeoff,
-                    color: Colors.pink,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 8.0),
-                  child: Text(
-                    'ISL',
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
-        const Padding(
-          padding: EdgeInsets.only(top: 20.0),
-          child: Text(
-            'Flight Ticket',
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 25.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    IconData icon;
+    String ticketClass;
+    Color ticketColor;
+
+    switch (ticketType) {
+      case 'Abierto':
+        icon = Icons.lock_open;
+        ticketClass = 'Abierto';
+        ticketColor = Colors.green;
+        break;
+      case 'Pendiente':
+        icon = Icons.hourglass_empty;
+        ticketClass = 'Pendiente';
+        ticketColor = Colors.orange;
+        break;
+      case 'Cerrado':
+        icon = Icons.lock;
+        ticketClass = 'Cerrado';
+        ticketColor = Colors.red;
+        break;
+      case 'Cancelado':
+        icon = Icons.cancel;
+        ticketClass = 'Cancelado';
+        ticketColor = Colors.grey;
+        break;
+      default:
+        icon = Icons.all_inclusive;
+        ticketClass = 'Todos';
+        ticketColor = theme.primary;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              ticketDetailsWidget(
-                  'Passengers', 'Hafiz M Mujahid', 'Date', '28-08-2022'),
-              Padding(
-                padding: const EdgeInsets.only(top: 12.0, right: 52.0),
-                child: ticketDetailsWidget('Flight', '76836A45', 'Gate', '66B'),
+              Container(
+                width: 120.0,
+                height: 25.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  border: Border.all(width: 1.0, color: ticketColor),
+                ),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(icon, color: ticketColor, size: 16.0),
+                      const SizedBox(width: 4.0),
+                      Text(
+                        ticketClass,
+                        style: TextStyle(color: ticketColor),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 12.0, right: 53.0),
-                child: ticketDetailsWidget('Class', 'Business', 'Seat', '21B'),
-              ),
+              Row(
+                children: [
+                  Text(
+                    'LHR',
+                    style: TextStyle(color: theme.secondary, fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Icon(
+                      Icons.flight_takeoff,
+                      color: theme.primary,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      'ISL',
+                      style: TextStyle(color: theme.secondary, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 80.0, left: 30.0, right: 30.0),
-          child: BarcodeWidget(
-            data: 'Ticket $ticketNumber',
-            barcode: Barcode.code128(),
-            width: 200,
-            height: 80,
-          ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(top: 10.0, left: 75.0, right: 75.0),
-          child: Text(
-            '0000 +9230 2884 5163',
-            style: TextStyle(
-              color: Colors.black,
+          const Padding(
+            padding: EdgeInsets.only(top: 20.0),
+            child: Text(
+              'Flight Ticket',
+              style: TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold),
             ),
           ),
-        ),
-        const SizedBox(height: 30),
-        const Text('         Developer: instagram.com/DholaSain')
-      ],
+          Padding(
+            padding: const EdgeInsets.only(top: 25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ticketDetailsWidget('Passengers', 'Hafiz M Mujahid', 'Date', '28-08-2022', theme),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12.0, right: 52.0),
+                  child: ticketDetailsWidget('Flight', '76836A45', 'Gate', '66B', theme),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12.0, right: 53.0),
+                  child: ticketDetailsWidget('Class', 'Business', 'Seat', '21B', theme),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 80.0, left: 30.0, right: 30.0),
+            child: BarcodeWidget(
+              data: 'Ticket $ticketNumber',
+              barcode: Barcode.code128(),
+              width: 200,
+              height: 80,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 75.0, right: 75.0),
+            child: Text(
+              '0000 +9230 2884 5163',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          const Text('         Developer: instagram.com/DholaSain')
+        ],
+      ),
     );
   }
 }
@@ -270,8 +314,7 @@ class BarcodeWidget extends StatelessWidget {
   }
 }
 
-Widget ticketDetailsWidget(String firstTitle, String firstDesc,
-    String secondTitle, String secondDesc) {
+Widget ticketDetailsWidget(String firstTitle, String firstDesc, String secondTitle, String secondDesc, ColorScheme theme) {
   return Row(
     mainAxisAlignment: MainAxisAlignment.spaceBetween,
     children: [
@@ -282,13 +325,13 @@ Widget ticketDetailsWidget(String firstTitle, String firstDesc,
           children: <Widget>[
             Text(
               firstTitle,
-              style: const TextStyle(color: Colors.grey),
+              style: TextStyle(color: theme.primary),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 4.0),
               child: Text(
                 firstDesc,
-                style: const TextStyle(color: Colors.black),
+                style: TextStyle(color: theme.secondary),
               ),
             )
           ],
@@ -301,13 +344,13 @@ Widget ticketDetailsWidget(String firstTitle, String firstDesc,
           children: [
             Text(
               secondTitle,
-              style: const TextStyle(color: Colors.grey),
+              style: TextStyle(color: theme.primary),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 4.0),
               child: Text(
                 secondDesc,
-                style: const TextStyle(color: Colors.black),
+                style: TextStyle(color: theme.secondary),
               ),
             )
           ],
